73da26921edfc6803391d04e38f760075803cd35
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-05 14:20:36 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,6 +34,11 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find_by_id(params[:id])
     @pet.update(params[:pet])
+    if params[:owner][:name] != ""
+      owner = Owner.create(params[:owner])
+      @pet.owner_id = owner.id
+      @pet.save
+    end
     redirect to "pets/#{@pet.id}"
   end
 end
